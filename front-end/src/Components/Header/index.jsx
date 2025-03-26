165eccab3715509e80ab5b0e353813f6f65ba737
decobot <capy@deco.cx>
2025-03-26 16:04:33 -0300

    AppEventos1.1

--- front-end/src/Components/Header/index.jsx
+++ front-end/src/Components/Header/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import './Style.css';
 
-const Header = ()  => (
-        <header>
-            <nav>
-                <ul>
-                    <li><Link to="/home">Home</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/calendario">Calendário</Link></li>
-                    <li><Link to="/eventos">Eventos</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+const Header = () => (
+    <header>
+        <nav>
+            <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/calendario">Calendário</Link></li>
+                <li><Link to="/mapa">Mapa de Eventos</Link></li>
+            </ul>
+        </nav>
+    </header>
+);
 
 export default Header;
